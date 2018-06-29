5583f705cc281968b072e7229fd3c5278ff1e175
Raoul v. R <vanruesc@users.noreply.github.com>
2018-06-29 12:38:07 +0200

    Version 1.2.1.
    Should actually be a breaking change, but the cli tool hasn't been around for long so it's fine.

--- build/inline-import.js
+++ build/inline-import.js
@@ -1,5 +1,5 @@
 /**
- * inline-import v1.2.0 build Wed Jun 27 2018
+ * inline-import v1.2.1 build Fri Jun 29 2018
  * https://github.com/vanruesc/inline-import
  * Copyright 2018 Raoul van Rüschen, Zlib
  */
